9a7f9da3f71612cfc2783810a8331c1811862895
Kas-MBOrijnland <74531912+Kas-MBOrijnland@users.noreply.github.com>
2022-07-04 01:40:00 +0200

    database

--- Imkerapp/imkerapp/database/migrations/2022_07_03_233224_create_comments_table.php
+++ Imkerapp/imkerapp/database/migrations/2022_07_03_233224_create_comments_table.php
@@ -13,15 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('comments', function (Blueprint $table) {
             $table->integer('id', 11);
+            $table->integer('post_id', 11);
             $table->string('user', 50);
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password', 255);
-            $table->rememberToken();
-            $table->foreignId('current_team_id')->nullable();
-            $table->string('profile_photo_path', 2048)->nullable();
+            $table->text('comment_data');
             $table->dateTime();
         });
     }
@@ -33,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('comments');
     }
 };
