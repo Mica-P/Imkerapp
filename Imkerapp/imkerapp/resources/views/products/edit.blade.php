f3623373af7f5409f881544c4343cd7867d71c27
Kas-MBOrijnland <74531912+Kas-MBOrijnland@users.noreply.github.com>
2022-07-06 15:10:22 +0200

    Aanpassen editpagina verslag - Mica

--- Imkerapp/imkerapp/resources/views/products/edit.blade.php
+++ Imkerapp/imkerapp/resources/views/products/edit.blade.php
@@ -4,7 +4,7 @@
     <div class="row">
         <div class="col-lg-12 margin-tb">
             <div class="pull-left">
-                <h2>Edit Product</h2>
+                <h2>Edit Verslag</h2>
             </div>
             <div class="pull-right">
                 <a class="btn btn-primary" href="{{ route('products.index') }}"> Back</a>
@@ -30,13 +30,13 @@
         <div class="row">
             <div class="col-xs-12 col-sm-12 col-md-12">
                 <div class="form-group">
-                    <strong>Name:</strong>
+                    <strong>Titel:</strong>
                     <input type="text" name="name" value="{{ $product->name }}" class="form-control" placeholder="Name">
                 </div>
             </div>
             <div class="col-xs-12 col-sm-12 col-md-12">
                 <div class="form-group">
-                    <strong>Detail:</strong>
+                    <strong>Beschrijving:</strong>
                     <textarea class="form-control" style="height:150px" name="detail" placeholder="Detail">{{ $product->detail }}</textarea>
                 </div>
             </div>
