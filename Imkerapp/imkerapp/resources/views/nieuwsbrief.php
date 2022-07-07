cdd8a953ce46fef9c846c31bbcc5b34b9bcc7d70
omer <1030517@mborijnland.nl>
2022-07-07 12:27:54 +0200

    nieuwsbrief.php

---
+++ Imkerapp/imkerapp/resources/views/nieuwsbrief.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title></title>
+    <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600"
+    />
+    <!-- https://fonts.google.com/specimen/Open+Sans -->
+    <link rel="stylesheet" href="{{url('css/navbar.css')}}" />
+
+    <!-- https://fontawesome.com/ -->
+    <link rel="stylesheet" href="{{url('css/bootstrap.min.css')}}" />
+    <!-- https://getbootstrap.com/ -->
+    <link rel="stylesheet" href="{{url('css/tooplate-style.css')}}" />
+</head>
+
+<body>
+
+<?php
+require_once ('../resources/views/navbar.blade.php');
+?>
+<br><br><br><br><br>
+
+<div class="main-container">
+
+    <article id="" class="">
+
+        <header class="post-header"><h1 class="page-title">Nieuwsbrief</h1></header><div class="post-content"><p>Dit is het nieuwsbrief van 7-7-2022:</p>
+            <p>Op 9 en 10 juli zijn er door heel Nederland Open Imkerijdag(en). Voor een kaartje met alle locaties waar je naartoe kan kan je hier kijken. En natuurlijk doet ook Imkervereniging Oegstgeest mee op onze bijenstand in Oud Poelgeest. Daar…</p>
+            <p>Van harte nodigen wij alle imkers uit de regio midden Nederland uit om de op de tweede zaterdag van april de voorjaarsbijenmarkt en honingkeuring in Austerlitz te komen bezoeken! De markt wordt weer echt opgezet voor en door imkers: behalve…</p>
+            <p>De cursus Specialist Bijenproducten biedt een afgeronde opleiding, die diep in gaat op de diverse bijenproducten: honing, was, stuifmeel, propolis, koninginnengelei, mede en ook enigszins op wetgeving, etikettering en promotie. De cursus wordt op zeven zaterdagen gegeven tussen november en…</p>
+            <p>Ter vervanging van de Open Imkerdagen 2021 heeft de NBV een serie video’s gemaakt over de bijen. We hebben ze verzameld op een handige overzichtspagina, klik hier!</p>
+            <p>Zondag 25 april was het gezellig druk op afstand op Oud Poelgeest. De vereniging sloot aan op de activiteiten van Oud Poelgeest zelf met het maken van raampjes voor de cursisten en mensen veel te vertellen over de ramen, kasten…</p>
+            <p>Helaas zijn er op de bijenstand Rijnweyde een aantal bijenkasten omgetrapt in het weekend an 23-25 april. De leden van de vereniging hebben alles in het werk gesteld om de schade zo goed mogelijk te herstellen en er zijn wat…</p>
+            <p>Op de ALV van 30 september is afscheid genomen van onze penningmeester Jan Persson. Karel Diephuis is verkozen als zijn opvolger. Karel is een beginnend imker en sinds 2 jaar lid van onze vereniging. Als 65plusser heeft hij tijd en…</p>
+            <p>De NBV krijgt momenteel dagelijks vragen over de gevolgen van de Coronacrisis. Leden worden geconfronteerd met praktische problemen, maar hebben ook vragen en/of zorgen over de (nabije) toekomst. Juist in deze onzekere tijd wil de NBV er zijn voor haar…</p>
+            <p>Het voorjaar staat op uitbarsten. Als imker hebben staat ons de komende maanden het nodige te doen. De overheidsmaatregelen om het Coronavirus onder de duim te houden, hebben ook effect op het imkeren. Wat ons te wachten staat, is moeilijk…</p>
+            <p>Hierbij treft u een berichtje aan met een link naar de brief die de Nederlandse imkerbonden vanmorgen hebben gestuurd naar het Ministerie van Landbouw, Natuur en Voedselkwaliteit. De maatregelen ten gevolge van het Corona-virus in Nederland volgen zich op. Ook…</p>
+        </div>
+    </article>
+
+
+</div>
+
+
+
+
+
+
+<?php
+require_once ('../resources/views/footer.blade.php');
+?>
+
+
+</body>
+</html>
